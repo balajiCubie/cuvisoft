75360b0c1921ca5d56350550e849d845ae1285f2
Balaji K <balajibala76394@gmail.com>
2024-06-17 11:14:56 +0530

    (Update CuviSoft Hero component and import for rebranding)

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 
 import BodyCard from "@/components/bodycard/BodyCard";
+import CuvisoftHero from "@/components/bodycard/CuvisoftHero";
 import SecBodyCard from "@/components/bodycard/SecBodyCard";
 import Footer from "@/components/footer/Footer";
 import Navbar from "@/components/navbar/Navbar";
@@ -9,7 +10,9 @@ import Navbar from "@/components/navbar/Navbar";
 export default function Home() {
   return (
    <div>
+
     <Navbar/>
+    <CuvisoftHero/>
     <BodyCard/>
     <SecBodyCard/>
     <Footer/>
