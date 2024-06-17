d5d687b9d190ad4b53ad2aa29d39136ca00077c2
Balaji K <balajibala76394@gmail.com>
2024-06-17 14:21:41 +0530

    (Update CuviSoft Hero component for rebranding including text, CSS, and animation)

--- components/bodycard/CuvisoftHero.tsx
+++ components/bodycard/CuvisoftHero.tsx
@@ -1,14 +1,17 @@
 // components/DockerHero.tsx
 import React from 'react';
+import  '../../app/textAnimation.css';
 
 const CuvisoftHero: React.FC = () => {
   return (
     <div className="flex  flex-col items-center justify-center bg-white text-center pt-16">
       <h2 className="text-xl font-semibold text-blue-700">Your life's work,powered by our life's work</h2>
+      <div className='container1'>
       <h1 className="mt-2  text-7xl font-bold text-blue-900 leading-snug  text-gradient">
       Transform your work with our powerful<br/>
       privacy-focused software suite,<br/> designed for businesses of all sizes.
       </h1>
+      </div>
 
       <p className="mt-10 mb-5 text-gray-600 font-semibold text-xl leading-8 tracking-wide">All-in-one suite <br />Run your entire business on Cuvisoft with our unified cloud software, <br />designed to help you break down silos between departments and increase organizational efficiency.</p>
 
